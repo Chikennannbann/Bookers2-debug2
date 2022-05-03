6e537492d9277e13eca04034fd99a7cbebb52b6a
Chikennannbann <kamsk517@gmail.com>
2022-05-03 07:00:21 +0000

    [finish]応用課題2(2)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,23 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
+    @booknew = Book.new
   end
 
   def index
     @books = Book.all
+    @booknew = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+    @booknew = Book.new(book_params)
+    @booknew.user_id = current_user.id
+    # 上の定義がないとuserが無いと出る。とは、bookカラムにはuser_idがあってそれが誰なのかを決めないと保存できないから。
+    # userのバリデーションが反応したわけではない！
+    if @booknew.save
+      redirect_to book_path(@booknew), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -31,9 +37,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
@@ -42,4 +48,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+  def ensure_correct_user
+   @book = Book.find(params[:id])
+   unless @book.user == current_user
+     redirect_to books_path
+   end
+  end
+
 end
